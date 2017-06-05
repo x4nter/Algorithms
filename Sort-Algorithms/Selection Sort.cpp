2f9fe487dd87b6a341c7d9d5210186ed65c5ad0f
x4nter <harmudharh@gmail.com>
2017-06-04 20:25:14 -0500

    Create Selection Sort.cpp

---
+++ Sort-Algorithms/Selection Sort.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <utility>
+
+int main(){
+    const int length = 5;
+    int input[length] = {40,30,10,50,20};
+
+    for(int i = 0; i < length; i++){
+        int smallest = i;
+        for(int j = i+1; j < length; j++){
+            if(input[j] < input[smallest])
+                smallest = j;
+        }
+        std::swap(input[smallest], input[i]);
+    }
+
+    for(int i = 0; i < length; i++){
+        std::cout << input[i] << " ";
+    }
+    
+    return 0;
+}
