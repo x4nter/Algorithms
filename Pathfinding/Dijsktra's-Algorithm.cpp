ffacd347aa462cc7bde4f6e816c3dd042caa55a5
x4nter <harmudharh@gmail.com>
2017-10-14 13:08:29 -0500

    Update Dijsktra's-Algorithm.cpp

--- Pathfinding/Dijsktra's-Algorithm.cpp
+++ Pathfinding/Dijsktra's-Algorithm.cpp
@@ -1,41 +1,35 @@
-/*Does not work properly yet
-Test Input:
-8 11
-0 2 2
-2 7 5
-7 5 7
-0 4 5
-2 3 8
-1 5 10
-4 6 3
-6 3 2
-3 5 5
-4 1 4
-1 3 4
-*/
 #include <iostream>
 #include <vector>
 #include <climits>
+#include <algorithm>
 
 using namespace std;
 
+void removeNode(vector<int>* v, int value){
+    vector<int>::iterator it = find(v->begin(), v->end(), value);
+    v->erase(it);
+}
+
 void dijsktra(vector<vector<int>> graph, int srcNode){
     int length = graph.size();
-    vector<int> distFromSrc(length, INT_MAX);
-    vector<bool> visited(length, false);
+    vector<int> distFromSrc(length, INT_MAX), queue;
     distFromSrc[srcNode] = 0;
-    for(int i = 0; i < length; i++){
+    for(int i = 0; i < length; i++)
+        queue.push_back(i);
+    while(!queue.empty()){
         int curNode;
-        for(int j = 0; j < length; j++){
+        for(int j = 0; j < queue.size(); j++){
             int min = INT_MAX;
             if(distFromSrc[j] < min){
                 min = distFromSrc[j];
                 curNode = j;
             }
         }
-        visited[curNode] = true;
-        for(int j = 0; j < length; j++){
-            if(!visited[j] && graph[curNode][j] && distFromSrc[curNode] + graph[curNode][j] < distFromSrc[j])
+        //removeNode(&queue, curNode);
+        vector<int>::iterator it = find(queue.begin(), queue.end(), curNode);
+        queue.erase(it);
+        for(int j = 0; j < queue.size(); j++){
+            if(graph[curNode][j] && distFromSrc[curNode] + graph[curNode][j] < distFromSrc[j])
                 distFromSrc[j] = distFromSrc[curNode] + graph[curNode][j];
         }
     }
@@ -44,15 +38,14 @@ void dijsktra(vector<vector<int>> graph, int srcNode){
 }
 
 int main(){
-    int nNodes, edges;
-    cin >> nNodes >> edges;
-    vector<vector<int>> matrix(nNodes, vector<int>(nNodes, 0));
-    for(int i = 0; i < edges; i++){
-    	int from, to, dis;
-    	cin >> from >> to >> dis;
-        matrix[from][to] = dis;
-        matrix[to][from] = dis;
-    }
+    vector<vector<int>> matrix = {{0,  0, 2, 0, 5,  0, 0, 0},
+                                  {0,  0, 0, 4, 4, 10, 0, 0},
+                                  {2,  0, 0, 8, 0,  0, 0, 5},
+                                  {0,  4, 8, 0, 0,  5, 2, 0},
+                                  {5,  4, 0, 0, 0,  0, 3, 0},
+                                  {0, 10, 0, 5, 0,  0, 0, 7},
+                                  {0,  0, 0, 2, 3,  0, 0, 0},
+                                  {0,  0, 5, 0, 0,  7, 0, 0}};
     dijsktra(matrix, 0);
     return 0;
 }
