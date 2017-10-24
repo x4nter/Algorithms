876a9806017d38fc6bc37bd68df09e04a9e8a1e5
x4nter <harmudharh@gmail.com>
2017-10-23 21:54:03 -0500

    Update Dijsktra's-Algorithm.cpp

--- Pathfinding/Dijsktra's-Algorithm.cpp
+++ Pathfinding/Dijsktra's-Algorithm.cpp
@@ -1,43 +1,45 @@
 #include <iostream>
 #include <vector>
 #include <climits>
-#include <algorithm>
 
 using namespace std;
 
-void removeNode(vector<int>* v, int value){
-    vector<int>::iterator it = find(v->begin(), v->end(), value);
-    v->erase(it);
-}
-
-void dijsktra(vector<vector<int>> graph, int srcNode){
-    int length = graph.size();
-    vector<int> distFromSrc(length, INT_MAX), queue;
-    distFromSrc[srcNode] = 0;
-    for(int i = 0; i < length; i++)
-        queue.push_back(i);
-    while(!queue.empty()){
-        int curNode;
-        for(int j = 0; j < queue.size(); j++){
-            int min = INT_MAX;
-            if(distFromSrc[j] < min){
-                min = distFromSrc[j];
-                curNode = j;
-            }
-        }
-        //removeNode(&queue, curNode);
-        vector<int>::iterator it = find(queue.begin(), queue.end(), curNode);
-        queue.erase(it);
-        for(int j = 0; j < queue.size(); j++){
-            if(graph[curNode][j] && distFromSrc[curNode] + graph[curNode][j] < distFromSrc[j])
-                distFromSrc[j] = distFromSrc[curNode] + graph[curNode][j];
+bool allTrue(vector<bool>* v){
+    bool b;
+    for(int i = 0; i < v->size(); i++){
+        if(!&v[i]){
+            b = false;
+            break;
         }
     }
-    for(int i = 0; i < distFromSrc.size(); i++)
-        cout << i << " : " << distFromSrc[i] << endl;
+    return b;
 }
 
-int main(){
+vector<int> dijkstra(vector<vector<int>> graph, int srcNode){
+    int length = graph.size();
+    vector<int> dist(length, INT_MAX);
+    vector<bool> visited(length, false);
+    dist[srcNode] = 0;
+    visited[srcNode] = true;
+    while(!allTrue(&visited)){
+        int curNode;
+        for(int i = 0; i < length; i++){
+            int min = INT_MAX;
+            if(dist[i] < min){
+                min = dist[i];
+                curNode = i;
+            }
+        }
+        visited[curNode] = true;
+        for(int i = 0; i < length; i++){
+            if(!visited[i] && graph[curNode][i] && graph[curNode][i] + dist[curNode] < dist[i])
+                dist[i] = dist[curNode] + graph[curNode][i];
+        }
+    }
+    return dist;
+}
+
+int main() {
     vector<vector<int>> matrix = {{0,  0, 2, 0, 5,  0, 0, 0},
                                   {0,  0, 0, 4, 4, 10, 0, 0},
                                   {2,  0, 0, 8, 0,  0, 0, 5},
@@ -46,6 +48,8 @@ int main(){
                                   {0, 10, 0, 5, 0,  0, 0, 7},
                                   {0,  0, 0, 2, 3,  0, 0, 0},
                                   {0,  0, 5, 0, 0,  7, 0, 0}};
-    dijsktra(matrix, 0);
+    vector<int> distFromSrc = dijkstra(matrix, 0);
+    for(int i = 0; i < distFromSrc.size(); i++)
+        cout << i << " : " << distFromSrc[i] << endl;
     return 0;
 }
